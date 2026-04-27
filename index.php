7e02195426aa9758ef0746146ba9ed6d7e8f33b6
Krankus-SSICR <krankus@stredniskola.com>
2026-04-27 19:35:23 +0200

    start

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+  </head>
+  <body>
+  <div>
+    <div>
+      <nav>
+        <div>
+          <h1>
+            <a href="/">Sportovní stravování</a>
+          </h1>
+        </div>
+        <ul>
+          <li>
+            <a href="#">
+              <span>Domů</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>O nás</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Kontakt</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  
+    <main>
+      <div>
+        <a href="#">Přihlásit se</a>
+        <a href="#">Zaregistrovat se</a>
+      </div>
+
+      <header>
+        <h2>Recepty</h2>
+        <h3>Pro hokejisty</h3>
+      </header>
+
+      <div>
+        <h4>Novinky</h4>
+  
+        <div>
+          <!-- galerie z karet -->
+          <div> 
+            <img src="img/steak.jpg" alt="Hovězí steak">
+            <div>
+              <span>Hovězí steak</span>
+              <span>Recept od Krankyho</span>
+            </div>
+          </div>
+        </div>
+
+        <h4>Nejoblíbenější</h4>
+
+        <div>
+          <!-- galerie z karet -->
+        </div>
+      </div>
+
+      <div>
+        <div>Načíst další</div>
+      </div>    
+    </main>
+  </div>
+  </body>
+</html>
